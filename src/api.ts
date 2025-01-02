7cb7ddf1836e2d171ab957fc6e72d98d52c5f120
Ami Lan <amitla2008@gmail.com>
2025-01-02 23:03:34 +0200

    hotfix: still have the same problem

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { PlaylistDetails, SongDetails } from './interfaces.ts';
 
-const backendServiceUrl = import.meta.env.VITE_BACKEND_SERVICE_URL || "https://127.0.0.1:8000";
+const backendServiceUrl = import.meta.env.VITE_BACKEND_SERVICE_URL || "http://127.0.0.1:8000";
 
 // combine fetch functions to 2 groups fetch normal requests and fetch mp3 file
 
